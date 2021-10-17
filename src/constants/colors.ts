00ebd7e5b2311d3114ad051922fb2190d7fc3b7b
echoi <off.echoi@gmail.com>
2021-10-17 19:28:49 +0900

    [Add] 색상 팔레트 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+type colorsType = {
+  [key: string]: string
+}
+const COLORS: colorsType = {
+  PrimaryBlue: '#0056f1',
+  DarkBlack: '#0e263e',
+  LightGray: '#eaebed',
+}
+
+export default COLORS
