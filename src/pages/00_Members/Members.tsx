1563442342ab7f370c4c9d19d7acde81eb034ad3
echoi <off.echoi@gmail.com>
2021-10-21 03:35:04 +0900

    [Add] 마크업 추가

--- src/pages/00_Members/Members.tsx
+++ src/pages/00_Members/Members.tsx
@@ -1,17 +1,30 @@
+import { memo } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Members() {
+function Members() {
   return (
     <div>
-      <p></p>
-      <h3>관리할 사용자를 고르세요.</h3>
+      <h2>가가님의 회원</h2>
+      <p>관리할 회원을 고르세요.</p>
       <ul className="member_list_wrap">
         <li className="member_list">
           <Link to="/main">
             <img src="" alt="회원 1 사진" />
           </Link>
         </li>
+        <li className="member_list">
+          <Link to="/main">
+            <img src="" alt="회원 2 사진" />
+          </Link>
+        </li>
+        <li className="member_list">
+          <Link to="/main">
+            <img src="" alt="회원 추가" />
+          </Link>
+        </li>
       </ul>
     </div>
   )
 }
+
+export default memo(Members)
