ff0678ba7de6fa80e97f0f658e589a1010bf687a
echoi <off.echoi@gmail.com>
2021-11-05 15:42:29 +0900

    [Fix] 타입 수정

--- src/components/BoardList/BoardList.tsx
+++ src/components/BoardList/BoardList.tsx
@@ -1,27 +1,40 @@
 /** @jsxImportSource @emotion/react */
-import { css, jsx } from '@emotion/react'
+import { css } from '@emotion/react'
 import { memo } from 'react'
 import { Link } from 'react-router-dom'
 
-type BoardListType = {
+type ContentType = {
   title: string
   date: string
   userName: string
   href: string // TODO: 오브젝트 형태로바꾸끼
   answerYN?: boolean // 질문, 답변 게시판용
 }
+type BoardListType = {
+  content: ContentType[]
+}
 
-function BoardList({ title, date, userName, href, answerYN }: BoardListType) {
+function BoardList({ content }: BoardListType) {
   return (
-    <li className="board_list" css={style}>
-      <div className="board_left">
-        <Link to={href} className={`board_title ${answerYN && 'badge_answer'}`}>
-          {title}
-        </Link>
-        <span className="board_user">{userName}</span>
-      </div>
-      <span className="board_date">{date}</span>
-    </li>
+    <ul css={style}>
+      {content.length ? (
+        content.map(({ title, date, userName, href, answerYN }: ContentType) => {
+          return (
+            <li className="board_list">
+              <div className="board_left">
+                <Link to={href} className={`board_title ${answerYN && 'badge_answer'}`}>
+                  {title}
+                </Link>
+                <span className="board_user">{userName}</span>
+              </div>
+              <span className="board_date">{date}</span>
+            </li>
+          )
+        })
+      ) : (
+        <li>게시글이 없습니다.</li>
+      )}
+    </ul>
   )
 }
 const style = css({
