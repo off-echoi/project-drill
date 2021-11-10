43f66fbc735c69e78391b6fc03f3a25b64d7cc0a
echoi <off.echoi@gmail.com>
2021-11-10 23:19:37 +0900

    [Add] 타이포 컴포넌트, 인포테이블 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 // 사용자 프로필 아바타
 export { default as Avatar } from './Avatar/Avatar'
 // 게시판류 목록
-export { default as BoardList } from '@/components/BoardList/BoardList'
+export { default as BoardList } from './BoardList/BoardList'
+// 사용자 정보
+export { default as InfoTable } from './InfoTable/InfoTable'
+// 텍스트
+export { default as Typo } from './Typo/Typo'
