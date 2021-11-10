bbcb11d2e6fd62a5d493635d2e018f2f7524db51
echoi <off.echoi@gmail.com>
2021-11-10 23:25:39 +0900

    [Add] 변경된 컴포넌트 사용으로 오류나는 부분 주석

--- src/pages/02_Task/TaskList.tsx
+++ src/pages/02_Task/TaskList.tsx
@@ -1,13 +1,7 @@
 import { memo } from 'react'
 import { BoardList } from '@/components'
 function TaskList() {
-  return (
-    <div>
-      <ul>
-        <BoardList title="잼잼ahr목" date="내일" userName="너야너" href="#" />
-      </ul>
-    </div>
-  )
+  return <div>{/* <BoardList title="잼잼ahr목" date="내일" userName="너야너" href="#" /> */}</div>
 }
 
 export default memo(TaskList)
