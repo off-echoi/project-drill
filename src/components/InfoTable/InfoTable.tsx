3c1f5718726fa4e6934835196b436a705a86bb85
echoi <off.echoi@gmail.com>
2021-11-10 23:21:40 +0900

    [Add] 인포테이블 컴포넌트 생성

---
+++ src/components/InfoTable/InfoTable.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { memo } from 'react'
+
+function InfoTable() {
+  return <div>테이블</div>
+}
+
+export default memo(InfoTable)
