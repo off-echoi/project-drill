269629eaf1b6a093562ad282804bd4e82cce77a1
echoi <off.echoi@gmail.com>
2021-11-10 23:20:57 +0900

    [Add] 변경된 Avatar 컴포넌트 적용

--- src/pages/00_Members/Members.tsx
+++ src/pages/00_Members/Members.tsx
@@ -1,30 +1,75 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import { memo } from 'react'
 import { Link } from 'react-router-dom'
+import { Avatar, Typo } from '@components/index'
+import ProfileImage from '@assets/profile01.jpg'
 
 function Members() {
   return (
-    <div>
-      <h2>가가님의 회원</h2>
-      <p>관리할 회원을 고르세요.</p>
+    <div css={style}>
+      <Typo type="title" addClassName="title">
+        안녕하세요! 가가님
+      </Typo>
+      <Typo type="text">관리할 회원을 고르세요.</Typo>
       <ul className="member_list_wrap">
         <li className="member_list">
-          <Link to="/main">
-            <img src="" alt="회원 1 사진" />
+          <Link to="/Info">
+            <Avatar name="회원1">
+              <img src={ProfileImage} alt="이미지" />
+            </Avatar>
           </Link>
         </li>
         <li className="member_list">
-          <Link to="/main">
-            <img src="" alt="회원 2 사진" />
+          <Link to="/Info">
+            <Avatar name="회원2">
+              <img src={ProfileImage} alt="이미지" />
+            </Avatar>
           </Link>
         </li>
         <li className="member_list">
-          <Link to="/main">
-            <img src="" alt="회원 추가" />
+          <Link to="/Info">
+            <Avatar name="회원3">
+              <img src={ProfileImage} alt="이미지" />
+            </Avatar>
           </Link>
+        </li>
+        <li className="member_list">
+          <Link to="/Info">
+            <Avatar name="회원4">
+              <img src={ProfileImage} alt="이미지" />
+            </Avatar>
+          </Link>
+        </li>
+        <li className="member_list">
+          <button className="add_member" onClick={() => alert('준비중인 기능입니다.')}>
+            회원추가
+          </button>
         </li>
       </ul>
     </div>
   )
 }
+
+const style = css`
+  .title {
+    margin-top: 20rem;
+  }
+  .member_list_wrap {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    width: 70%;
+    margin: 0 auto;
+  }
+  .member_list {
+    margin-top: 20rem;
+  }
+  .add_member {
+    width: 80rem;
+    height: 80rem;
+    border-radius: 100%;
+  }
+`
 
 export default memo(Members)
