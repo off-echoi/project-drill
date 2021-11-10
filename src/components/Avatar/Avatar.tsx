78692f842b4cfab964fc0e85e9a227552043caa4
echoi <off.echoi@gmail.com>
2021-11-10 23:17:05 +0900

    [Add] 아바타 컴포넌트 타입 추가

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,21 +1,76 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import COLORS from '@constants/colors'
 import { memo, ReactElement } from 'react'
+import { join } from 'path'
 
 type AvatarType = {
   children?: ReactElement<HTMLImageElement>
   name: string
   date?: string // 클래스 날짜
+  type?: 'list' | 'photo'
 }
 
-function Avatar({ children, name, date }: AvatarType) {
+function Avatar({ children, name, date, type = 'photo' }: AvatarType) {
   return (
-    <div css={style}>
-      {children}
-      <span>{name}</span>
-      {date && <span>{date}</span>}
+    <div css={() => style(type)}>
+      <div className="img_wrap">{children}</div>
+      <div>
+        <span className="member_name">{name}</span>
+        {date && <span className="member_schedule">{date}</span>}
+      </div>
     </div>
   )
 }
-const style = css({})
+const style = (type: 'list' | 'photo') => {
+  let addStyle = ''
+  if (type === 'list') {
+    addStyle = `
+      display: flex;
+      align-items: center;
+      text-align: left;
+      .img_wrap {
+        width: 60rem;
+        height: 60rem;
+        margin: 0;
+      }
+      .member_name {
+        margin-top: 0;
+        margin-left:10rem;
+      }
+    `
+  } else {
+    addStyle = `
+      text-align: center;
+    `
+  }
+  const commonstyle = css`
+    .img_wrap {
+      overflow: hidden;
+      width: 80rem;
+      height: 80rem;
+      margin: 0 auto;
+      border-radius: 100%;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    .member_name {
+      display: block;
+      font-size: 16rem;
+      margin-top: 10rem;
+    }
+    .member_schedule {
+      display: block;
+      font-size: 14rem;
+      margin-top: 5rem;
+      color: ${COLORS.SecondGray};
+    }
+    ${addStyle}
+  `
+  return commonstyle
+}
+
 export default memo(Avatar)
