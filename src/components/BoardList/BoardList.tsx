57caaf492b9f84c235aec9e602483e114c849d40
echoi <off.echoi@gmail.com>
2021-11-18 01:47:37 +0900

    [Add] 보드 스타일 추가

--- src/components/BoardList/BoardList.tsx
+++ src/components/BoardList/BoardList.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import COLORS from '@constants/colors'
 import { memo } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -21,13 +22,16 @@ function BoardList({ content }: BoardListType) {
         content.map(({ title, date, userName, href, answerYN }: ContentType) => {
           return (
             <li className="board_list">
+              {answerYN && <span className="badge_answer">V</span>}
               <div className="board_left">
-                <Link to={href} className={`board_title ${answerYN && 'badge_answer'}`}>
+                <Link to={href} className="board_title">
                   {title}
                 </Link>
-                <span className="board_user">{userName}</span>
               </div>
-              <span className="board_date">{date}</span>
+              <div className="board_right">
+                <span className="board_user">{userName}</span>
+                <span className="board_date">{date}</span>
+              </div>
             </li>
           )
         })
@@ -37,11 +41,54 @@ function BoardList({ content }: BoardListType) {
     </ul>
   )
 }
-const style = css({
-  boxSizing: 'border-box',
-  width: 300,
-  height: 200,
-  border: '10px solid blue',
-})
+const style = css`
+  border-top: 2rem solid ${COLORS.SecondGray};
+  border-bottom: 2rem solid ${COLORS.SecondGray};
+  font-size: 15rem;
+  .board_list {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10rem 5rem;
+    & + .board_list {
+      border-top: 1rem solid ${COLORS.LightGray};
+    }
+  }
+  .board_left {
+    flex: 1;
+  }
+  .board_left a {
+    overflow: hidden;
+    display: -webkit-box;
+    width: 95%;
+    padding-left: 5rem;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    line-height: 1.3;
+  }
+  .badge_answer {
+    display: block;
+    top: 0;
+    bottom: 0;
+    width: 17rem;
+    height: 17rem;
+    margin: auto;
+    border-radius: 100%;
+    background: ${COLORS.PrimaryBlue};
+    color: #ffffff;
+    text-align: center;
+    line-height: 17rem;
+    font-size: 10rem;
+  }
+  .board_right {
+    flex: 0 75rem;
+    font-size: 12rem;
+  }
+  .board_right span {
+    display: block;
+    padding: 2.5rem 0;
+  }
+`
 
 export default memo(BoardList)
