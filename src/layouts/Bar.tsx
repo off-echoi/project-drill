cc0704eb7784b8543832ec0f78389b9393f57259
echoi <off.echoi@gmail.com>
2021-11-19 20:45:01 +0900

    [Add] 하단 앱바 스타일 추가, 아이콘 마크업 추가

--- src/layouts/Bar.tsx
+++ src/layouts/Bar.tsx
@@ -1,36 +1,46 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import COLORS from '@constants/colors'
 import { memo } from 'react'
 import { NavLink } from 'react-router-dom'
-import COLORS from '@constants/colors'
+import { ReactComponent as Task } from '@assets/barIcons/icon_task.svg'
+import { ReactComponent as Qna } from '@assets/barIcons/icon_qna.svg'
+import { ReactComponent as Info } from '@assets/barIcons/icon_info.svg'
+import { ReactComponent as Notice } from '@assets/barIcons/icon_notice.svg'
+import { ReactComponent as Analysis } from '@assets/barIcons/icon_analysis.svg'
 
 function Bar() {
   return (
     <section css={style}>
       <ul className="bar_wrap">
         <li>
-          <NavLink to="/task" activeClassName="on" className="nav">
-            과제
+          <NavLink to="/task" activeClassName="on" className="nav nav_task">
+            <Task />
+            <span>과제</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/qna" activeClassName="on" className="nav">
-            질문
+          <NavLink to="/qna" activeClassName="on" className="nav nav_qna">
+            <Qna />
+            <span>질문</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/info" activeClassName="on" className="nav">
-            정보
+          <NavLink to="/info" activeClassName="on" className="nav nav_info">
+            <Info />
+            <span>정보</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/notice" activeClassName="on" className="nav">
-            공지
+          <NavLink to="/notice" activeClassName="on" className="nav nav_notice">
+            <Notice />
+            <span>공지</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/analysis" activeClassName="on" className="nav">
-            분석
+          <NavLink to="/analysis" activeClassName="on" className="nav nav_ana">
+            <Analysis />
+            <span>분석</span>
           </NavLink>
         </li>
       </ul>
@@ -43,27 +53,34 @@ const style = css`
   bottom: 0;
   left: 0;
   width: 100vw;
-  border-top: 1rem solid ${COLORS.DarkBlack};
+  background: white;
+  border-top: 1rem solid ${COLORS.LightGray};
   .bar_wrap {
     display: flex;
     width: 100%;
     justify-content: space-between;
     li {
       flex: 1;
-      & + li {
-        border-left: 1rem solid ${COLORS.DarkBlack};
-      }
     }
   }
   .nav {
     width: 100%;
     height: 50rem;
-    line-height: 50rem;
+    padding: 5rem 0;
     text-align: center;
-    font-size: 16rem;
+    font-size: 12rem;
     &.on {
       color: ${COLORS.PrimaryBlue};
     }
+  }
+  svg {
+    display: block;
+    width: auto;
+    height: 20rem;
+    margin: 0 auto 8rem;
+  }
+  .on svg {
+    fill: ${COLORS.PrimaryBlue};
   }
 `
 
