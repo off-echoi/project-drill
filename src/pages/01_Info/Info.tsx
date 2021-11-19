67b2112573ad70bdcbd26733d89a3c4f10d3dc53
echoi <off.echoi@gmail.com>
2021-11-19 20:46:30 +0900

    [Add] 헤더 추가, 회원정보 영역 추가

--- src/pages/01_Info/Info.tsx
+++ src/pages/01_Info/Info.tsx
@@ -1,36 +1,47 @@
-import { Avatar } from '@/components'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { Avatar, InfoTable } from '@components/index'
 import { memo } from 'react'
+import Header from '@layouts/Header'
 import ProfileImg from '@assets/profile01.jpg'
+
 function Info() {
   return (
-    <section>
-      설정버튼
-      <div>
-        <Avatar name="김뫄뫄" date="2006. 10. 20">
-          <img src={ProfileImg} alt="프로필 이미지" />
-        </Avatar>
-      </div>
-      <table>
-        <tr>
-          <th>시간 : </th>
-          <td>월, 수 17:00 ~ 19:00</td>
-        </tr>
-        <tr>
-          <th>과목 : </th>
-          <td>영어</td>
-        </tr>
-        <tr>
-          <th>정보 : </th>
-          <td>뫄뫄중학교 3학년</td>
-        </tr>
-        <tr>
-          <td rowSpan={2}>
-            <p>기타 메모사항~~</p>
-          </td>
-        </tr>
-      </table>
-    </section>
+    <>
+      <Header pageName="회원정보">
+        <button>셋팅</button>
+      </Header>
+      <section className="header_section" css={style}>
+        <div className="avartar_wrap">
+          <Avatar name="김회원">
+            <img src={ProfileImg} alt="프로필 이미지" />
+          </Avatar>
+        </div>
+        <InfoTable>
+          <tr>
+            <th>시간 </th>
+            <td>월, 수 17:00 ~ 19:00</td>
+          </tr>
+          <tr>
+            <th>과목 </th>
+            <td>영어</td>
+          </tr>
+          <tr>
+            <th>정보 </th>
+            <td>대와중학교 3학년</td>
+          </tr>
+          <tr>
+            <th>기타</th>
+            <td>기타 메모사항</td>
+          </tr>
+        </InfoTable>
+      </section>
+    </>
   )
 }
-
+const style = css`
+  .avartar_wrap {
+    margin: 40rem 0 20rem;
+  }
+`
 export default memo(Info)
