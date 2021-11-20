db69a63f6dc604be2603228adab46bd03d44f8a4
echoi <off.echoi@gmail.com>
2021-11-20 16:53:46 +0900

    [Add] 뒤로가기 기능 및 케이스 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,25 +2,45 @@
 import { css } from '@emotion/react'
 import COLORS from '@constants/colors'
 import { memo, ReactElement, useCallback } from 'react'
+import { useHistory } from 'react-router'
 import { Button, Typo } from '@components/index'
 import { ReactComponent as Menu } from '@assets/icon_menu.svg'
+import { ReactComponent as Back } from '@assets/icon_back.svg'
 
 type HeaderType = {
+  type?: 'menu' | 'back'
   pageName: string
   children?: ReactElement<HTMLButtonElement> | ReactElement<HTMLButtonElement>[]
 }
 
-function Header({ pageName, children }: HeaderType) {
+function Header({ type = 'menu', pageName, children }: HeaderType) {
+  const history = useHistory()
+  // 사이드 네비 함수
   const fnNav = useCallback(() => {
+    //TODO:
     console.log('>>>>')
   }, [])
+
+  // 뒤로가기
+  const fnGoBack = useCallback(() => {
+    history.goBack()
+  }, [history])
+
   return (
     <header css={style}>
-      <Button onClick={fnNav} type="icon" addClassName="btn_menu">
-        <Menu />
-        <span className="hidden">메뉴</span>
-      </Button>
-      <Typo type="title" addClassName="pageTitle">
+      {type === 'menu' && (
+        <Button onClick={fnNav} type="icon" addClassName="btn_left">
+          <Menu />
+          <span className="hidden">메뉴</span>
+        </Button>
+      )}
+      {type === 'back' && (
+        <Button onClick={fnGoBack} type="icon" addClassName="btn_left">
+          <Back />
+          <span className="hidden">뒤로가기</span>
+        </Button>
+      )}
+      <Typo type="title" addClassName="page_title">
         {pageName}
       </Typo>
       {children && <div className="right_btn_wrap">{children}</div>}
@@ -35,7 +55,7 @@ const style = css`
   height: 45rem;
   border-bottom: 1rem solid ${COLORS.LightGray};
   background: #fff;
-  .btn_menu {
+  .btn_left {
     position: absolute;
     left: 0;
     top: 0;
@@ -43,7 +63,7 @@ const style = css`
     z-index: 1;
     margin: auto;
   }
-  .pageTitle {
+  .page_title {
     font-size: 18rem;
     line-height: 45rem;
   }
