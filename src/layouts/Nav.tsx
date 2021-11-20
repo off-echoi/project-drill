ae8b72e62c8b2af0bbc2a6a1f52326a07d8e4e6e
echoi <off.echoi@gmail.com>
2021-11-20 23:02:39 +0900

    [Add] 네비게이션 동작 추가

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -1,58 +1,89 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import COLORS from '@constants/colors'
-import { memo } from 'react'
-import { Avatar } from '@/components'
+import { memo, useCallback } from 'react'
+import { Avatar, Button } from '@components/index'
 import { NavLink } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { popupControll } from '@/reducers/popup'
+import { RootState } from '@reducers/index'
 import MyProfile from '@assets/profile02.jpg'
+import { ReactComponent as Close } from '@assets/icon_close.svg'
 
 function Nav() {
+  const { NAV_STATE } = useSelector((state: RootState) => state.popup)
+
+  const dispatch = useDispatch()
+  const fnCloseNav = useCallback(() => {
+    dispatch(popupControll({ type: 'NAV_STATE', payload: false }))
+  }, [dispatch])
+
   return (
-    <nav css={style}>
-      <section className="info_section">
-        <Avatar name="최선생" type="list">
-          <img src={MyProfile} alt="내 사진" />
-        </Avatar>
-      </section>
-      <ul>
-        <li className="link_list">
-          <NavLink to="/" activeClassName="on">
-            내 정보 수정
-          </NavLink>
-        </li>
-        <li className="link_list">
-          <NavLink to="/members" activeClassName="on">
-            다른 회원 선택
-          </NavLink>
-        </li>
-        <li className="link_list">
-          <NavLink to="/memberDelete" activeClassName="on">
-            회원삭제
-          </NavLink>
-        </li>
-        <li className="link_list">
-          <button onClick={() => alert('로그아웃 되었습니다.')}>로그아웃</button>
-        </li>
-        <li className="link_list">
-          <NavLink to="/withdraw" activeClassName="on">
-            서비스 탈퇴
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav css={style} className={NAV_STATE ? 'on' : ''}>
+        <Button type="icon" addClassName="btn_close" onClick={fnCloseNav}>
+          <Close />
+          <span className="hidden">네비게이션 닫기</span>
+        </Button>
+        <section className="info_section">
+          <Avatar name="최선생" type="list">
+            <img src={MyProfile} alt="내 사진" />
+          </Avatar>
+        </section>
+        <ul>
+          <li className="link_list">
+            <NavLink to="/" activeClassName="on">
+              내 정보 수정
+            </NavLink>
+          </li>
+          <li className="link_list">
+            <NavLink to="/members" activeClassName="on">
+              다른 회원 선택
+            </NavLink>
+          </li>
+          <li className="link_list">
+            <NavLink to="/memberDelete" activeClassName="on">
+              회원삭제
+            </NavLink>
+          </li>
+          <li className="link_list">
+            <button onClick={() => alert('로그아웃 되었습니다.')}>로그아웃</button>
+          </li>
+          <li className="link_list">
+            <NavLink to="/withdraw" activeClassName="on">
+              서비스 탈퇴
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      {NAV_STATE && <div className="dim" css={dimStyle}></div>}
+    </>
   )
 }
 
 const style = css`
   position: fixed;
-  left: 0;
+  left: -350rem;
   top: 0;
-  z-index: 10;
+  z-index: 30;
   width: 250rem;
   height: 100vh;
   padding: 0 20rem;
   background: white;
   box-shadow: 0px 0 20px rgba(0, 0, 0, 0.3);
+  transition: all 0.2s;
+  &.on {
+    left: 0;
+  }
+  .btn_close {
+    position: absolute;
+    right: -40rem;
+    top: 0rem;
+    z-index: 30;
+    svg {
+      fill: #fff;
+    }
+  }
   .info_section {
     border-bottom: 1px solid ${COLORS.LightGray};
     padding: 20rem 0;
@@ -69,5 +100,14 @@ const style = css`
       }
     }
   }
+`
+const dimStyle = css`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 25;
+  background: rgba(0, 0, 0, 0.5);
 `
 export default memo(Nav)
