cb3300b045976dbc2f471df20a9bb39a16bbd439
echoi <off.echoi@gmail.com>
2021-11-20 16:52:54 +0900

    [Add] 인풋 컴포넌트 생성

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,44 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { ChangeEventHandler, InputHTMLAttributes, memo } from 'react'
+
+interface InputType extends InputHTMLAttributes<HTMLInputElement> {
+  labelText: string
+  type: 'text'
+  name: string
+  value: string
+  id: string
+  onChange?: ChangeEventHandler
+  addClassName?: string
+}
+
+function Input({ labelText, type, name, id, value, onChange, addClassName, ...props }: InputType) {
+  return (
+    <div css={style} className={addClassName}>
+      <label htmlFor={id}>{labelText}</label>
+      <input type={type} name={name} id={id} value={value} onChange={onChange} {...props} />
+    </div>
+  )
+}
+
+const style = css`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 14rem;
+  label {
+    display: inline-block;
+    min-width: 80rem;
+  }
+  input {
+    padding: 3rem;
+    border: 1rem solid #888888;
+    font-size: 14rem;
+    width: calc(100% - 80rem);
+    &:read-only {
+      border: none;
+      pointer-events: none;
+    }
+  }
+`
+export default memo(Input)
