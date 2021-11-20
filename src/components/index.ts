e11f98f6bbf9a4215740653df4ddff050b01e3eb
echoi <off.echoi@gmail.com>
2021-11-20 16:51:35 +0900

    [Add] 게시판 상세류, 인풋 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,13 @@
 export { default as Avatar } from './Avatar/Avatar'
 // 게시판류 목록
 export { default as BoardList } from './BoardList/BoardList'
+// 게시판류 읽기
+export { default as BoardTable } from './BoardTable/BoardTable'
 // 사용자 정보
 export { default as InfoTable } from './InfoTable/InfoTable'
 // 텍스트
 export { default as Typo } from './Typo/Typo'
 // 버튼
 export { default as Button } from './Button/Button'
+// 인풋
+export { default as Input } from './Input/Input'
