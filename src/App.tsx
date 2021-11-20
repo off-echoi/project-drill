2bdea94583d4b3d7131f97d0e0a215700e26f800
echoi <off.echoi@gmail.com>
2021-11-20 23:34:39 +0900

    [Add] 페이지 진입시 스크롤 0으로 조정, 네비 닫음,

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import Router from '@router/index'
+import { useEffect } from 'react'
+import RouteIndex from '@router/index'
 import Bar from '@layouts/Bar'
-import { BrowserRouter } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import Nav from '@layouts/Nav'
-import ModalPortal from './ModalPortal'
+import ModalPortal from '@layouts/ModalPortal'
+import { useDispatch } from 'react-redux'
+import { popupControll } from '@/reducers/popup'
 
 function App() {
+  const { pathname } = useLocation()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    // 페이지 전환 시 항상 스크롤 값 0
+    document.body.scrollTop = 0
+    // 페이지 전환 시 햄버거 네비게이션 닫음
+    dispatch(popupControll({ type: 'NAV_STATE', payload: false }))
+  }, [dispatch, pathname])
+
   return (
-    <BrowserRouter>
-      <Router />
-      <Bar />
-      <ModalPortal>
-        <Nav />
-      </ModalPortal>
-    </BrowserRouter>
+    <>
+      <RouteIndex />
+      <>{pathname === '/members' ? <></> : <Bar />}</>
+      <Nav />
+      {/* 팝업영역 */}
+      <ModalPortal></ModalPortal>
+    </>
   )
 }
 
