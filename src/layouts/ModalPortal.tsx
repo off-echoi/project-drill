8ecbced8707b66e02cdc3b74de3d2eceb8e2cb95
echoi <off.echoi@gmail.com>
2021-11-20 22:59:46 +0900

    [Update] 경로 변경

--- src/layouts/ModalPortal.tsx
+++ src/layouts/ModalPortal.tsx
@@ -1,13 +1,13 @@
-import { ReactElement } from 'react'
+import { memo } from 'react'
 import ReactDOM from 'react-dom'
 
 type PortalType = {
-  children?: ReactElement<HTMLElement> | ReactElement<HTMLElement>[]
+  children?: JSX.Element | JSX.Element[] | null
 }
 
-const ModalPortal = ({ children }: PortalType) => {
+function ModalPortal({ children }: PortalType) {
   const portal: HTMLDivElement = document.getElementById('modal') as HTMLDivElement
   return ReactDOM.createPortal(children, portal)
 }
 
-export default ModalPortal
+export default memo(ModalPortal)
