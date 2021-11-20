f007c8abd3464308c69ce05606fd382e001fffd2
echoi <off.echoi@gmail.com>
2021-11-20 23:20:00 +0900

    [Add] popup 리듀서 추가

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import counter from './counter'
 import user from './user'
+import popup from './popup'
 
 const rootReducer = combineReducers({
   counter,
+  popup,
   user,
 })
 
