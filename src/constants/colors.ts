3a02a87d0ec8f2b0d3da39944b1114a760650450
ec_choi <ecchoi@allmytour.com>
2023-11-07 14:32:28 +0900

    docs : 기존 색상 삭제예정 주석 추가 & 타입 정리

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,11 +1,12 @@
 // type colorsType = {
 //   readonly [key: string]: string
 // }
+// TODO: 삭제 예정
 enum COLORS {
-  PrimaryBlue = '#0056f1',
+  PrimaryBlue = '#2778e9',
   DarkBlack = '#0e263e',
   LightGray = '#eaebed',
   SecondGray = '#555555',
 }
 
-export default COLORS
+export default COLORS;
