d0a7d8463f6c76c0a36fc4d687c801427b1f7ece
ecchoi <off.echoi@gmail.com>
2023-11-08 03:43:43 +0900

    feat : ctaBox 컴포넌트 추가

---
+++ src/components/ctaBox/CTABox.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { theme } from 'styles/theme';
+import { Typography } from 'components';
+
+interface CTABoxProps {
+  title?: string;
+  content: string;
+}
+
+const StyledDiv = styled.div`
+  background: ${theme.color.grayg8};
+  border-radius: ${theme.rounded.md};
+  border: 1px solid ${theme.color.gray8};
+  padding: ${theme.spacing.xxl};
+  .title {
+    margin-bottom: ${theme.spacing.lg};
+  }
+`;
+
+export const CTABox = ({ title, content }: CTABoxProps) => {
+  return (
+    <StyledDiv>
+      {title && (
+        <Typography typoType="sub2" align="center" color={theme.color.sub2} className="title">
+          {title}
+        </Typography>
+      )}
+      {content && (
+        <Typography typoType="body2" align="center" color={theme.color.gray5}>
+          {content}
+        </Typography>
+      )}
+    </StyledDiv>
+  );
+};
