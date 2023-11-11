f7f1e186b375e01350338dfb1e3bd280a218f67a
ecchoi <off.echoi@gmail.com>
2023-11-11 18:48:26 +0900

    rename : 버튼 폴더 대소문자 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,22 +3,29 @@ export { Avatar } from './avatar/Avatar';
 // 게시판류 목록
 export { BoardList } from './boardList/BoardList';
 // 게시판류 읽기
-export { default as BoardTable } from './BoardTable/BoardTable';
+export { default as BoardTable } from './BoardTable/BoardTable'; // TODO 삭제 및 BoradTableV2 로 대체예정
 // 사용자 정보
-export { default as InfoTable } from './InfoTable/InfoTable';
+export { default as InfoTable } from './InfoTable/InfoTable'; // TODO 삭제 및 로 대체예정
+//
+export { BoradTableV2 } from './table/BoradTable'; // 게세판 컨탠츠용 테이블
+export { ListTable } from './table/ListTable'; // 게시판 리스트용 테이블
 // 텍스트
 export { Typo } from './typo/Typo'; // TODO 삭제 및 Typography 로 대체예정
 export { Typography } from './typo/Typography';
 export { ReaderOnlyText } from './typo/ReaderOnlyText';
+
 // * From 요소
 // ** 버튼
-export { default as Button } from './button/Button';
-export { ButtonV2 } from './button/Button_v2';
+export { default as Button } from './button/Button'; // TODO 삭제 및 ButtonV2 로 대체예정
+export { ButtonV2 } from './button/ButtonV2';
 export { IconButton } from './button/IconButton';
 // ** 인풋
-export { default as Input } from './input/Input'; // TODO 삭제 및 Input_v2 로 대체예정
-export { InputV2 } from './input/Input_v2';
+export { default as Input } from './input/Input'; // TODO 삭제 및 InputV2 로 대체예정
+export { InputV2 } from './input/InputV2';
 export { Label } from './input/Label';
+export { TextArea } from './input/TextArea';
+export { Radio } from './input/Radio';
+export { CheckBox } from './input/CheckBox';
 
 // * 일반
 // ** cta 박스
@@ -52,5 +59,6 @@ export { UserIcon } from './icons/UserIcon';
 
 // 레이아웃
 export { BottomNav } from './layout/BottomNav'; // 하단 앱바
-export { Header } from './layout/Header'; // 하단 앱바
+export { Header } from './layout/Header'; // 헤더
+export { SideNav } from './layout/SideNav'; // 좌측 메뉴
 export { NoContent } from './layout/NoContent'; // 컨텐츠 없음 레이아웃
