87f94b327d853f9f393db9b679cc7e0c743c9831
ecchoi <off.echoi@gmail.com>
2023-11-12 20:02:55 +0900

    feat : ctaBox 컴포넌트 아이콘 추가

--- src/components/ctaBox/CTABox.tsx
+++ src/components/ctaBox/CTABox.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import { theme } from 'styles/theme';
-import { Typography } from 'components';
+import { InfoIcon, MinusCircleIcon, Typography } from 'components';
 
 interface CTABoxProps {
   title?: string;
   content: string;
+  ctaType?: 'info' | 'error' | 'basic';
+  className?: string;
 }
 
 const StyledDiv = styled.div`
@@ -12,23 +14,36 @@ const StyledDiv = styled.div`
   border-radius: ${theme.rounded.md};
   border: 1px solid ${theme.color.gray8};
   padding: ${theme.spacing.xxl};
+  margin-bottom: ${theme.spacing.xxl};
+  text-align: center;
+  svg {
+    margin-bottom: ${theme.spacing.sm};
+  }
   .title {
     margin-bottom: ${theme.spacing.lg};
   }
 `;
 
-export const CTABox = ({ title, content }: CTABoxProps) => {
+export const CTABox = ({ title, content, ctaType = 'info', className }: CTABoxProps) => {
   return (
-    <StyledDiv>
+    <StyledDiv className={className}>
+      {ctaType !== 'basic' && (
+        <>
+          {ctaType === 'info' && <InfoIcon size={20} color={theme.color.gray5} />}
+          {ctaType === 'error' && <MinusCircleIcon size={20} color={theme.color.accent} />}
+        </>
+      )}
       {title && (
         <Typography typotype="sub2" align="center" color={theme.color.sub2} className="title">
           {title}
         </Typography>
       )}
       {content && (
-        <Typography typotype="body2" align="center" color={theme.color.gray5}>
-          {content}
-        </Typography>
+        <>
+          <Typography typotype="body2" align="center" color={theme.color.gray5}>
+            {content}
+          </Typography>
+        </>
       )}
     </StyledDiv>
   );
